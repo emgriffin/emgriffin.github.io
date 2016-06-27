3b942a25e2832b131cc6952b8cc406600a91d20c
Errin Griffin <erring@hotmail.com>
2016-06-27 15:42:20 -0400

    positioning
    Learned about static, fixed, absolute, and relative positioning.
    Inserted a youtube video with iFrame. Learned how to center absolute
    elements. Learned about z-indexing.

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -9,7 +9,7 @@ $(".navbar-collapse ul li a[href^='#']").on('click', function(e) {
    var top = 50;
 
    if (w <= 767) {
-       top = 150;
+       top = 120;
    }
 
    // animate
@@ -30,11 +30,31 @@ $("a.navbar-brand[href^='#']").on('click', function(e) {
    e.preventDefault();
 
    // store hash
-   var hash = this.hash;
+   var hash = this.hash; 
 
    // animate
    $('html, body').animate({
-       scrollTop: $(hash).offset().top -top
+       scrollTop: $(hash).offset().top -50
+     }, 300, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
+});
+
+$("#back-to-top a[href^='#']").on('click', function(e) {
+
+   // prevent default anchor click behavior
+   e.preventDefault();
+
+   // store hash
+   var hash = this.hash; 
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top -50
      }, 300, function(){
 
        // when done, add hash to url
